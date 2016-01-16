a5db14410634cdbe67be37ecc2df9ff445698085
mvaude <maxime.vaude@scality.com>
2016-01-16 17:35:04 +0100

    mod listMessages (getAttachments not working)

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
         process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json';
 
+var authCli = "";
+
 // Load client secrets from a local file.
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
     if (err) {
@@ -39,7 +41,7 @@ function authorize(credentials, callback) {
       getNewToken(oauth2Client, callback);
     } else {
       oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client, listMessages);
+      callback(oauth2Client, 'has:attachment', writeIt);
     }
   });
 }
@@ -71,7 +73,7 @@ function getNewToken(oauth2Client, callback) {
       }
       oauth2Client.credentials = token;
       storeToken(token);
-      callback(oauth2Client);
+      callback(oauth2Client, 'has:attachment', writeIt);
     });
   });
 }
@@ -107,12 +109,14 @@ function storeToken(token) {
  * @param  {String} query String used to filter the Messages listed.
  * @param  {Function} callback Function to call when the request is complete.
  */
+/*
 function listMessages(auth) {
   var list = [];
   var gmail = google.gmail('v1');
   gmail.users.messages.list({
     auth: auth,
     userId: 'me',
+    q: "has:attachment",
   }, function (err, response) {
     var messages = response.messages;
     console.log("Messages: %s", messages.length);
@@ -120,29 +124,114 @@ function listMessages(auth) {
       var message = messages[i];
       console.log('- %s', message.id);
     }
-    console.log("Size: %s", response.resultSizeEstimate);
     console.log("Size: %s", response.nextPageToken);
+    allPages(auth, response.nextPageToken);
   });
 }
 
-function allPages(token) {
+function allPages(auth, token) {
+  var gmail = google.gmail('v1');
+  gmail.users.messages.list({
+    auth: auth,
+    userId: 'me',
+    q: "has:attachment",
+    pageToken: token,
+  }, function (err, response) {
+    var messages = response.messages;
+    console.log("Messages: %s", messages.length);
+    for (i = 0; i < messages.length; i++) {
+      var message = messages[i];
+      console.log('- %s', message.id);
+    }
+    console.log("Size: %s", response.nextPageToken);
+    allPages(response.nextPageTocken);
+    });
+    }
+/*
+/**
+ * Retrieve Messages in user's mailbox matching query.
+ *
+ * @param  {String} userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @param  {String} query String used to filter the Messages listed.
+ * @param  {Function} callback Function to call when the request is complete.
+ */
+
+function callWithParams(method, params, callback) {
+  var gmail = google.gmail('v1');
+  if (method == 'messages.list') {
+    
+    gmail.users.messages.list(params, callback);
+  }
+  else if (method == 'message.get') {
+    gmail.users.messages.get(params, callback);
+  }
+  else if (method == 'attachment.get') {
+    gmail.users.messages.attachments.get(params, callback);
+  }
 }
 
-/**
- * List emails with attached files for each label
- *
- * @param {labels}
- */
-/* function listAttachedEmails(auth, labels, callback) {
-   var gmail = google.gmail('v1');
-   gmail.users.messages.list({
-   auth: auth,
-   userId: 'me',
-   }, function (err, response) {
-   if (err) {
-   console.log('The API returned an error: ' + err);
-   return;
-   }
-   var messages = resposone.messages
-   });
-   }*/
+function listMessages(auth, query, callback) {
+  autCli = auth;
+  var getPageOfMessages = function(params, result) {
+      callWithParams('messages.list', params, function(err, resp) {
+      result = result.concat(resp.messages);
+      var nextPageToken = resp.nextPageToken;
+      if (nextPageToken) {
+        params = {
+          auth: auth,
+          userId: 'me',
+          pageToken: nextPageToken,
+          q: query
+        };
+        getPageOfMessages(params, result);
+      } else {
+        callback(result);
+      }
+      });
+  };
+  var params = {
+    auth: auth,
+    userId: 'me',
+    q: query
+  };
+  getPageOfMessages(params, []);
+}
+
+function writeIt(result) {
+  console.log("Number of messages: %s", result.length);
+  for (var i = 0; i < result.length; i++) {
+    var params = {
+      auth: authCli,
+      id: result[0].id,
+      userId: 'me',
+    };
+    callWithParams('message.get', params, addCallback);
+  }
+  console.log("END");
+}
+
+function getAttachments (message, callback) {
+  parts = message.payload.parts;
+  for (var i = 0; i < parts.length; i++) {
+    var part = parts[i];
+    if (part.filename && part.filename.length > 0) {
+      var attachId = part.body.attachmentId;
+      var params = {
+        auth: authCli,
+        userId: 'me',
+        id: attachId,
+        messageId: message.id
+      }
+      callWithParams('attachment.get', params, callback); 
+    }
+  }
+}
+
+function addCallback(err, res) {
+  getAttachments(res, writeOnDisk);
+}
+
+function writeOnDisk (filename, mimeType, attachment) {
+  console.log("filename: %s", filename);
+}
